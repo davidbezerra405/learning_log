e540fcc843d387f3484fab1db0a3b2e198ebf167
david <davidbezerra405@hotmail.com>
2021-08-04 10:09:52 -0300

    Initial commit

---
+++ users/urls.py
@@ -0,0 +1,16 @@
+"""Define padrões de URL para users"""
+
+from django.conf.urls import url
+from django.contrib.auth.views import login
+
+from . import views
+
+urlpatterns = [
+    # Página de login
+    url(r'^login/$', login, {'template_name': 'users/login.html'}, name='login'),
+    # Página de logout
+    url(r'^logout/$', views.logout_view, name='logout'),
+    # Página de cadastro
+    url(r'^register/$', views.register, name='register'),
+]
+
